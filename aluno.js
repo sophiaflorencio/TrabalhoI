ae9e4a8a804045ac35c47e950f9262079ef17caa
NatanElertSilva <natanelertdasilva@gmail.com>
2024-06-26 10:34:57 -0300

    Update aluno.js
    commit alunos

---
+++ aluno.js
@@ -0,0 +1,29 @@
+const sequelize = require("sequelize");
+const banco = require("./banco")
+const professor = require("./professor")
+
+var aluno = banco.conexao.define(
+    "aluno",
+    {
+        id:{
+            type:sequelize.INTEGER.UNSIGNED,
+            primaryKey: true,
+            autoIncrement:true
+        },
+        nome:{
+            type:sequelize.STRING,
+            allowNull:false
+        },
+        idade:{
+            type:sequelize.INTEGER.UNSIGNED,
+            allowNull:false
+        },
+        serie:{
+            type:sequelize.INTEGER.UNSIGNED,
+            allowNull:false
+        }
+    },
+    { timestamps: false }
+)
+
+module.exports = {alunos}
