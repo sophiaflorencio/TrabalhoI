06456473e61a7f90f65ae206d6fd509f56dd4104
sophiaflorencio <160235254+sophiaflorencio@users.noreply.github.com>
2024-06-28 07:22:36 -0300

    Create aluno.js

---
+++ node/aluno.js
@@ -0,0 +1,32 @@
+const sequelize = require("sequelize");
+const banco = require("./banco")
+const professor = require("../professor")
+const alunos = [
+    {id: 1, nome: "Alessandra", idade: 17,  serie: "info22"}
+]
+
+var aluno = banco.conexao.define(
+    "aluno",
+    {
+        id:{
+            type:sequelize.INTEGER.UNSIGNED,
+            primaryKey: true,
+            autoIncrement:true
+        },
+        nome:{
+            type:sequelize.STRING,
+            allowNull:false
+        },
+        idade:{
+            type:sequelize.INTEGER.UNSIGNED,
+            allowNull:false
+        },
+        serie:{
+            type:sequelize.INTEGER.UNSIGNED,
+            allowNull:false
+        }
+    },
+    { timestamps: false }
+)
+
+module.exports = {aluno}
